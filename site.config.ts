b45e602960f53a992e7510588d9d9999fcdc8bc2
hongling2511 <134196080+hongling2511@users.noreply.github.com>
2023-09-21 15:51:18 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '88d39058b3664b20b55e6affb300b4d7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Personal Home of hongling',
+  domain: 'https://www.notion.so/Personal-Home-88d39058b3664b20b55e6affb300b4d7',
+  author: 'hongling',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
